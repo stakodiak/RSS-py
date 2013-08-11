8cd58fb8c37dfcd49638fbe7947de768bd807432
Alex Stachowiak <astac1@umbc.edu>
2013-08-11 17:23:43 -0400

    Update RSS.py

--- RSS.py
+++ RSS.py
@@ -3,30 +3,6 @@ import urllib2 as u2
 import xml.etree.ElementTree as ET
 
 
-def main ():
-    # Test Google News.
-    print "Google News feed:"
-    url = 'https://news.google.com/news/feeds?hl=en&gl=us&authuser=0&q=RSS&num=10&um=1&ie=UTF-8&output=rss'
-    feed = RSS (url)
-    for i in feed:
-        print i['title']
-    print ""
-
-    # Get HN feed.
-    print "HN frontpage:"
-    url = 'https://news.ycombinator.com/rss'
-    feed = RSS (url)
-    for i in feed:
-        print i['title']
-    print ""
-
-    # Try a subreddit.
-    print "r/compsci RSS:"
-    url = 'http://www.reddit.com/r/compsci/.rss'
-    feed = RSS (url)
-    for i in feed:
-        print i['title']
-
 def RSS (url):
     # Returns a list of RSS-dict elements.
     feed = list()
@@ -41,6 +17,3 @@ def RSS (url):
         feed.append (elem)
 
     return feed
-
-if __name__ == '__main__':
-    main()
