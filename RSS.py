136c1edf5fe3db60703746b66f2587b0baf54c9b
alex <astac1@umbc.edu>
2013-12-27 16:11:32 -0500

    Better styled script.

--- RSS.py
+++ RSS.py
@@ -2,18 +2,14 @@
 import urllib2 as u2
 import xml.etree.ElementTree as ET
 
-
-def RSS (url):
-    # Returns a list of RSS-dict elements.
+def RSS(url):
     feed = list()
-
     # Get items from RSS channel.
-    RSS_feed = u2.urlopen (url).read()
+    RSS_feed = u2.urlopen(url).read()
     root = ET.fromstring(RSS_feed)
-    for i in root.findall ('./channel/item'):
+    for i in root.findall('./channel/item'):
         elem = dict()
         for j in i:
             elem [j.tag] = j.text
-        feed.append (elem)
-
+        feed.append(elem)
     return feed
